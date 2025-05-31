35f51859243cc743be1212afca8faada78d8b8c7
Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
2025-05-31 14:44:32 +0200

    spi: geni-qcom: Drop geni_icc_en/disable() calls
    These functions are being deprecated, stop calling them.
    
    Signed-off-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>

--- drivers/spi/spi-geni-qcom.c
+++ drivers/spi/spi-geni-qcom.c
@@ -1135,16 +1135,11 @@ static int __maybe_unused spi_geni_runtime_suspend(struct device *dev)
 {
 	struct spi_controller *spi = dev_get_drvdata(dev);
 	struct spi_geni_master *mas = spi_controller_get_devdata(spi);
-	int ret;
 
 	/* Drop the performance state vote */
 	dev_pm_opp_set_rate(dev, 0);
 
-	ret = geni_se_resources_off(mas->se);
-	if (ret)
-		return ret;
-
-	return geni_icc_disable(mas->se);
+	return geni_se_resources_off(mas->se);
 }
 
 static int __maybe_unused spi_geni_runtime_resume(struct device *dev)
@@ -1152,10 +1147,6 @@ static int __maybe_unused spi_geni_runtime_resume(struct device *dev)
 	struct spi_controller *spi = dev_get_drvdata(dev);
 	struct spi_geni_master *mas = spi_controller_get_devdata(spi);
 	int ret;
-
-	ret = geni_icc_enable(mas->se);
-	if (ret)
-		return ret;
 
 	ret = geni_se_resources_on(mas->se);
 	if (ret)
